71cf9cbf43099c2f1e472d1e4b12ea9537a16e3a
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-06-02 03:48:45 -0400

    Update extension.js

--- structinspector/extension.js
+++ structinspector/extension.js
@@ -12,23 +12,71 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "structinspector" is now active!');
+	console.log('Congratulations, your extension "structinspector" is now activated!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('structinspector.helloWorld', function () {
+	let ShowInformationWindow = vscode.commands.registerCommand('structinspector.ShowInformationWindow', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from StructInspector!');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(ShowInformationWindow);
+
+	// List of languages we want to support
+	const languages = ['c', 'cpp','csharp', 'javascript','lua','python', 'typescript','vb', 'rust'];
+
+	// Register a hover provider for each language
+	for (const language of languages) {
+		let registerHoverProviderLanguage = vscode.languages.registerHoverProvider(language, {
+			provideHover(document, position, token) {
+				token;
+				const word = document.getText(document.getWordRangeAtPosition(position));
+				if (word === 'StructInspector') {
+					let markdown = new vscode.MarkdownString();
+					markdown.appendText('StructInspector: The current state of your data structure');
+					switch (language) {
+						case 'c':
+							markdown.appendCodeblock('struct MyStruct {\n\tint key;\n\tchar value;\n};', language);
+							break;
+						case 'cpp':
+							markdown.appendCodeblock('struct MyStruct {\n\tint key;\n\tchar value;\n};', language);
+							break;
+						case 'csharp':
+							markdown.appendCodeblock('public class MyStruct {\n\tpublic int key;\n\tpublic char value;\n}', language);
+							break;
+						case 'javascript':
+							markdown.appendCodeblock('let MyStruct = {\n\t"key":"value"\n};', language);
+							break;
+						case 'lua':
+							markdown.appendCodeblock('local MyStruct = {\n\tkey = "value"\n}', language);
+							break;
+						case 'python':
+							markdown.appendCodeblock('class MyStruct:\n\tdef __init__(self):\n\t\tself.key = "value"', language);
+							break;
+						case 'rust':
+							markdown.appendCodeblock('struct MyStruct {\n\tkey: String,\n\tvalue: String\n}', language);
+							break;
+						case 'vb':
+							markdown.appendCodeblock('Public Class MyStruct\n\tpublic key As Integer\n\tpublic value As Char\nEnd Class', language);
+							break;
+					}
+					return new vscode.Hover(markdown);
+				}
+			}
+		});
+
+		context.subscriptions.push(registerHoverProviderLanguage);
+	}
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() {	
+	console.log('Extension "structinspector" is now de-activated!');
+}
 
 module.exports = {
 	activate,
